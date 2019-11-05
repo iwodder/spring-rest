9406dd8dc8bcc1d71340358c7469a1f13dead8ab
iwodder <ian.wodder@gmail.com>
2019-11-04 19:49:08 -0600

    i18n with Spring, working on HATEOAS

--- src/main/java/com/example/take/some/rest/springrest/user/UserAPI.java
+++ src/main/java/com/example/take/some/rest/springrest/user/UserAPI.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import org.springframework.web.util.UriComponents;
 
 import javax.validation.Valid;
 import java.net.URI;
@@ -22,6 +21,7 @@ public class UserAPI {
         if (u == null) {
             throw new UserNotFoundException("id-" + id);
         }
+
         return u;
     }
 
