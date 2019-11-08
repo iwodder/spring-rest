52d7e1a7c77d2807fa4e76d0ac7163705ab1b836
iwodder <ian.wodder@gmail.com>
2019-11-08 06:27:56 -0600

    Resolved issues with Swagger preventing service from starting.

--- src/main/java/com/example/take/some/rest/springrest/user/User.java
+++ src/main/java/com/example/take/some/rest/springrest/user/User.java
@@ -1,10 +1,15 @@
 package com.example.take.some.rest.springrest.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+@ApiModel(description = "A description for the user")
 public class User {
     private DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM-dd-yyyy");
     private int id;
@@ -12,6 +17,9 @@ public class User {
     @Size(min=5, max = 100, message = "Name must be between 5 and 100 characters.")
     @Pattern(regexp = "[a-z][A-Z]", message = "Name must only contain alphanumeric characters.")
     private String name;
+
+    @Past
+    @ApiModelProperty(notes="Birth date cannot be in the past")
     private LocalDate birthDate;
 
     private User() { }
