45db425fe8d584a4dca496637cfe11a019865026
iwodder <ian.wodder@gmail.com>
2019-11-07 19:45:46 -0600

    Accept XML, i18n, and some swagger docs

--- src/main/java/com/example/take/some/rest/springrest/user/UserAPI.java
+++ src/main/java/com/example/take/some/rest/springrest/user/UserAPI.java
@@ -1,6 +1,8 @@
 package com.example.take.some.rest.springrest.user;
 
+import net.bytebuddy.asm.Advice;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -8,12 +10,16 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+import java.util.Locale;
 
 @RestController
 public class UserAPI {
 
     @Autowired
     UserDataService dataService;
+
+    @Autowired
+    ResourceBundleMessageSource messageSource;
 
     @GetMapping(path="/users/{id}")
     public User getUserById(@PathVariable int id) {
@@ -44,5 +50,10 @@ public class UserAPI {
     public ResponseEntity deleteUser(@PathVariable int id) {
         dataService.deleteUser(id);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/users/hello")
+    public String helloi18n(@RequestHeader(value = "Accept-Language", required = false) Locale locale) {
+        return messageSource.getMessage("good.morning.message", null, locale);
     }
 }
